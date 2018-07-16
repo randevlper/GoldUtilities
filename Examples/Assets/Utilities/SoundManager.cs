3f10ada31263c055c48806deac4d7c5f735f8bc5
Randall Hines <randallhines0@gmail.com>
2018-07-16 11:07:57 -0700

    -Add Examples

---
+++ Examples/Assets/Utilities/SoundManager.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour {
+	public static SoundManager instance;
+	private void Awake() {
+		
+	}
+}
